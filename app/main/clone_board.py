93cc343766bfa11ea8e07022c5e68fb4cbb5bff1
praisebak <praisebak@naver.com>
2021-06-10 03:27:10 +0900

    add board modify and delete should add password 2

--- app/main/clone_board.py
+++ app/main/clone_board.py
@@ -1,18 +1,17 @@
 from threading import main_thread
 from flask import Blueprint, request, render_template, flash, redirect, url_for, g,session
+from pymysql import NULL
 from app.module.dbModule import Database
 from app.forms.forms import ContentAddForm, UserAddCheck
 from datetime import datetime
 
 clone_board_bp = Blueprint('clone_board',__name__,url_prefix='/clone_board')
 
-@clone_board_bp.route('/list',methods=['POST','GET'])
+@clone_board_bp.route('/list')
 def list():
     db = Database()
     content_list = db.executeAll("SELECT * FROM board_content_table")
-    print(content_list)
     return render_template('/main/board.html',content_list=content_list)
-
 
 
 @clone_board_bp.route('/add',methods=['POST','GET'])
@@ -29,4 +28,34 @@ def add():
         db.commit()
         return redirect(url_for("clone_board.list"))
         
+
     return render_template('/main/board_add.html',form=form)
+
+@clone_board_bp.route('/list/<int:board_content_idx>/')
+def content(board_content_idx):
+    db = Database()
+    data = db.executeAll("""SELECT * FROM board_content_table WHERE board_content_idx = %s""" %str(board_content_idx))
+    return render_template('/main/board_content.html',content=data)
+
+@clone_board_bp.route('/del/<int:board_content_idx>/')
+def delContent(board_content_idx):
+    db = Database()
+    db.execute("""DELETE FROM board_content_table WHERE board_content_idx = %s""" %str(board_content_idx))
+    db.commit()
+    return redirect(url_for("clone_board.list"))
+
+    
+@clone_board_bp.route('/modify/<int:board_content_idx>/',methods=['POST','GET'])
+def modify(board_content_idx):
+    db = Database()
+    data = db.executeAll("""SELECT * FROM board_content_table WHERE board_content_idx = %s""" %str(board_content_idx))
+
+    if request.method == 'POST':
+        form = ContentAddForm()
+        if form.validate_on_submit():
+            db.execute("""UPDATE board_content_table SET write_time='%s',board_content_title='%s',board_content='%s' WHERE board_content_idx = '%s'""" % (datetime.now(),form.content_title.data,form.content_text.data,str(data[0]['board_content_idx']) ))
+            db.commit() 
+            return redirect(url_for('clone_board.content',board_content_idx=board_content_idx))
+    else:
+        form = ContentAddForm(content_title=data[0]['board_content_title'],content_text=data[0]['board_content'])
+    return render_template('/main/board_add.html',form=form,board_content_idx=board_content_idx)
